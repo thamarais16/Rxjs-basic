f0b7a07a502c757a234ead624f27d4b78976c323
thamarais16 <thamarais16@gmail.com>
2021-08-11 12:10:19 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -10,11 +10,30 @@ import {
 } from 'rxjs';
 
 /**
- * @description Observable is the function that convert
+ * @description Observable is the function that convert or
  * @description Tap Operator is useful for logging, debuuging the stream. It does not modify the stream any way.
  * 
 */
-
+new Observable((observe)=>{
+  setTimeout(()=>{
+    observe.next(1);
+  },2000);
+  setTimeout(()=>{
+    observe.next(2);
+  },4000);
+  setTimeout(()=>{
+    observe.complete();
+  },6000);
+}).pipe(
+  tap(
+    val => {
+      console.log("After " + val);
+    },
+    () => {
+      console.log("completed");
+    }
+  ),
+) .subscribe(val => console.log(val));
 
 
 
