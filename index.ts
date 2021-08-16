9044df73e50ab9e9575f02d4d50be94d323cc849
thamarais16 <thamarais16@gmail.com>
2021-08-16 20:24:37 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -10,11 +10,15 @@ import {
 } from 'rxjs';
 
 /**
- * @description Observable is the function that convert or
+ * @description Observable is the function that convert ordinary stream of data into observable streaam of data. it emit value asynchronously from stream asynchronously. emits error when stream errors out. emit complete signal when stream completes.
  * @description Tap Operator is useful for logging, debuuging the stream. It does not modify the stream any way.
- * 
+ * @description of Operator creates observable from argumnents we pass in it. we can pass any number of argumnets.it emit each argumnets separately one after other.
+ * @description from Operator creates observable from argumnents we pass in it. we can pass only one argument in it.It iterates over argument and emits each value.
 */
-new Observable((observe)=>{
+
+
+/**1 */
+Observable.create((observe)=>{
   setTimeout(()=>{
     observe.next(1);
   },2000);
@@ -34,6 +38,15 @@ new Observable((observe)=>{
     }
   ),
 ) .subscribe(val => console.log(val));
+
+/**2 */
+let obs = of([1,2,3], 100, "Hello");
+
+obs.subscribe(
+  (val)=> {console.log(val)},
+  (err)=> {console.log(err)},
+  ()=> {console.log("completed")},
+)
 
 
 
